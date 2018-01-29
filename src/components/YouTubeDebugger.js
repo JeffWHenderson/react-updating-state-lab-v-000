8a858e0a89eedf12eef018c62306eafdc8568540
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-29 18:22:52 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -17,7 +17,7 @@ class YouTubeDebugger extends React.Component {
     }
   }
   handleBitrateClick = () => {
-    this.setState({...this.state.settings, bitrate: 100})
+    this.setState({settings: {...this.state.settings, bitrate: 100}})
   }
 
   handleResolutionClick = () => {
