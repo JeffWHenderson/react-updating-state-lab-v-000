640d1f9582d0705ffc77e2fb1e1c33eab29ba9d8
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-29 18:07:03 +0000

    automatically backed up by learn

---
+++ src/components/YouTubeDebugger.js
@@ -0,0 +1,28 @@
+// Code YouTubeDebugger Component Here
+import React from 'react'
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super()
+
+    this.state = () => {
+       errors: [], 
+       user: null, 
+       settings: {
+         bitrate: 8, 
+         video: { 
+           resolution: '1080p' 
+         } 
+       } 
+    }  
+  }
+
+  
+  render() {
+    return (
+      <p>Hello World</p>
+    )
+  }
+}
+
+export default YouTubeDebugger
