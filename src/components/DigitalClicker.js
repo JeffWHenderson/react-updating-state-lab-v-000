a3f9cfb374415b12e14b0cd76db5c1032969a7f1
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-29 17:56:59 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -3,6 +3,13 @@ import React from 'react'
 
 class DigitalClicker extends React.Component {
 
+  this.state = () => {
+    timesClicked: 0
+  }
+
+  handleClick = () => {
+    this.setState({timesClicked: this.state.timesClicked+1})
+  }
   render() {
     return (
       <p>Hello World</p>
